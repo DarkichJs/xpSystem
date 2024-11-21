fff93446b02589bbc1308c1f32e3de85b3c2d26a
Anton <82670270+DarkichJs@users.noreply.github.com>
2024-11-21 12:33:03 +0200

    profile

--- Commands/Main/profile.js
+++ Commands/Main/profile.js
@@ -28,15 +28,36 @@ module.exports = {
         lvl: 0,
       });
     }
+
+    function getXpForNextLevel(level) {
+        return 35 + (level * 15); 
+    }
+
+    function generateLevelBar(xp, level) {
+      const totalBars = 20;
+      const xpForNextLevel = getXpForNextLevel(level);
+      const filledBars = Math.floor((xp / xpForNextLevel) * totalBars);
+      const emptyBars = totalBars - filledBars;
+      return `${'█'.repeat(filledBars)}${'░'.repeat(emptyBars)}`;
+    }
+
+    const levelBar = generateLevelBar(user_find.xp, user_find.lvl);
+    const xpForNextLevel = getXpForNextLevel(user_find.lvl);
+    const xpPerMessage = 7; 
+    const messagesToNextLevel = Math.ceil((xpForNextLevel - user_find.xp) / xpPerMessage);
+
     const embed = new EmbedBuilder()
-      .setAuthor({name: `Profile - ${user.tag}`, iconURL: 'https://cdn.discordapp.com/emojis/1305439461949313035.webp?size=96'})
+      .setAuthor({name: `Profile - ${user.tag}`, iconURL: 'https://cdn.discordapp.com/emojis/1305439515518701599.webp?size=96'})
       .setDescription(`
         \`\`\`User: ${user.globalName}\`\`\` 
+        \`\`\`Messages: ${user_find.messages} -> ${messagesToNextLevel}\`\`\`
         \`\`\`XP: ${user_find.xp}\`\`\`
         \`\`\`Level: ${user_find.lvl}\`\`\`
-        \`\`\`Messages: ${user_find.messages}/200\`\`\`
+        \`\`\`${levelBar}\`\`\`
         `)
       .setColor("#303136")
+      .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+
     interaction.reply({ embeds: [embed] });
   },
 };
--- Events/messageCreate/messageCreate.js
+++ Events/messageCreate/messageCreate.js
@@ -16,10 +16,35 @@ module.exports = async (client, message) => {
         user = new User({
             userID: message.author.id,
             messages: 1,
-            status: false
+            xp: 0,
+            lvl: 0,
         });
     } else {
         user.messages += 1;
+    }
+
+    function getXpForNextLevel(level) {
+        return 35 + (level * 15); 
+    }
+
+    user.xp = (user.xp || 0) + 7; 
+
+    let xpForNextLevel = getXpForNextLevel(user.lvl);
+    while (user.xp >= xpForNextLevel) {
+        user.xp -= xpForNextLevel;
+        user.lvl += 1;
+        xpForNextLevel = getXpForNextLevel(user.lvl);
+        const levelUpEmbed = new EmbedBuilder()
+            .setAuthor({name: `LEVEL UP - ${message.author.username}`, iconURL: 'https://cdn.discordapp.com/emojis/1299860143999156224.webp?size=96&animated=true'})
+            .setDescription(`**${message.author}, you leveled up to level \`${user.lvl}\`!**`)
+            .setColor("#303136")
+            .setThumbnail(message.author.displayAvatarURL({ dynamic: true }));
+
+        message.channel.send({ embeds: [levelUpEmbed] }).then(sentMessage => {
+            setTimeout(() => {
+                sentMessage.delete().catch(console.error);
+            }, 10000); 
+        });
     }
 
     try {
